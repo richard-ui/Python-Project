c68e6be8700043afc1fcbfe904b2677cba206a0e
richard-ui <richard-jones96@hotmail.co.uk>
2021-02-07 19:21:22 +0000

    Deployment to Heroku

---
+++ run.py
@@ -0,0 +1,58 @@
+import os
+import json
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
+@app.route("/")
+def index():
+    return render_template("index.html", page_title="Home")
+
+
+@app.route("/about")
+def about():
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/about/<key_name>")  # passing key name from about
+def about_member(key_name):
+    key = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == key_name:   # retrive url from dictionary.
+                key = obj
+    return render_template("key.html", key=key)  # renders new key.html file.
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
+    return render_template("contact.html", page_title="Contact")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html", page_title="Careers")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
